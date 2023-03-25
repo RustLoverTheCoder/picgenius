6ec2a8aee0b4bb01a9e2f747788199484617603e
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-03-25 16:29:22 +0800

    feat: add login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,9 @@
 import { getDictionary } from "@/dictionaries";
 import Ui from "./ui";
 
-const Page = async ({ params: { lang } }) => {
-  const dict = await getDictionary(lang);
-  console.log("lang", lang, dict);
+const Page = async () => {
+  // const dict = await getDictionary(lang);
+  // console.log("lang", lang, dict);
 
   return (
     <>
