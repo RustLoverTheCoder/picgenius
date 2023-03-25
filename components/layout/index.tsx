4e4ddd11b386412b23166103dc92335fc913a4e7
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-03-25 23:31:03 +0800

    feat: add Suspense

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,11 +1,13 @@
-import React, { FC } from "react";
+import React, { FC, Suspense } from "react";
 import Header from "../header";
 
 const HomeLayout = ({ children }: any) => {
   return (
     <div className="w-full h-auto min-h-full">
       <Header />
-      <main className="mt-[60px] h-auto min-h-[calc(100vh-62px)]">{children}</main>
+      <main className="mt-[60px] h-auto min-h-[calc(100vh-62px)]">
+        <Suspense fallback={<></>}>{children}</Suspense>
+      </main>
     </div>
   );
 };
