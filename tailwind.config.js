cd2ecb8602bf5dfa5f0a3b43537cf586f217961c
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-03-28 09:43:22 +0800

    feat: add old color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}', // Note the addition of the `app` directory.
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./ui/**/*.{js,ts,jsx,tsx}"
+    './src/**/*.{js,ts,jsx,tsx}',
+    './ui/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        'base-100': '#1e1f22',
-        'base-200': "#2b2d31",
-        "primary-100": "#5865f2",
+        'base-100': '#05020E',
+        'base-200': '#2b2d31',
+        'primary-100': '#5865f2',
       },
     },
   },
